6b58f48c540be11b0e2c5f2ba0f603ab482512ec
Thalia Melo <bsm.thalia@gmail.com>
2025-10-16 19:16:49 -0300

    Admin - Cadastrar e editar maquinas
    Cadastrar, editar e remover maquinas já implementados, com acesso ao banco e tudo funcionando, só falta ajustes de interface.

---
+++ modelos/maquina.py
@@ -0,0 +1,104 @@
+""" -- Model maquina.py
+Responsável pela persistência (consultas SQL), mapeamento simples entre linha do banco ↔ objeto Python. 
+Todas as operações CRUD com o MySQL. Onde cuidamos da integridade dos dados e do uso do conector (conexao_bd.conectar()). """
+
+from dataclasses import dataclass   #cria um "molde" para a classe (COM init, repr)
+from typing import Optional, List
+from banco_de_dados.conexao_bd import conectar
+import mysql.connector
+
+@dataclass
+class Maquina:
+    id_maquina: Optional[int]
+    id_lavanderia: int
+    codigo_maquina: str
+    tipo_maquina: str    # lavadora ou secadora
+    status_maquina: str  # livre, em_uso ou manutenção
+    capacidade: str
+
+
+# -- Cadastra Máquina no Banco
+def criar_maquina(maquina: Maquina) -> int:
+    
+    sql = " INSERT INTO maquina (id_lavanderia, codigo_maquina, tipo_maquina, status_maquina, capacidade) VALUES (%s, %s, %s, %s, %s)"
+    #gera o comando sql de Insert (a query), não coloquei id.maquina porque esta com autoincrement no banco
+    conn = conectar()  #chama a função conectar para abrir a conexão com o banco
+    try:
+        cur = conn.cursor()  #abre o cursor
+        cur.execute(sql, (maquina.id_lavanderia, maquina.codigo_maquina, maquina.tipo_maquina, maquina.status_maquina, maquina.capacidade))  #executa a query com os valores
+        conn.commit() #"salva" a alteração
+        new_id = cur.lastrowid  #pega o id gerado pelo autoincrment
+        cur.close()  #fecha o cursor
+        return new_id
+    finally:
+        conn.close()  #fecha a conexão
+
+
+# -- Atualiza Máquina no Banco
+def atualizar_maquina(id_maquina: int, campos: dict) -> bool:   #campos: dicionario com colunas e dados a atualizar
+    
+    set_clause = ", ".join(f"{k} = %s" for k in campos.keys())  #organiza os campos em uma "string" set_clause
+    params = list(campos.values()) + [id_maquina]  #orgazina os parametros
+    
+    sql = f"UPDATE maquina SET {set_clause} WHERE id_maquina = %s" #gera o comando sql de UPDATE
+    conn = conectar() #abre a conexao com o banco
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, params) #executa o comando sql
+        conn.commit()
+        affected = cur.rowcount #indica quantas linhas alteradas, 
+        cur.close()
+        return affected > 0  #retorna true se alterou algo, false se não 
+    finally:
+        conn.close() #fecha a conexão
+
+
+# -- Deletar Máquinas no Banco
+def deletar_maquina(id_maquina: int) -> bool:
+    
+    sql = "DELETE FROM maquina WHERE id_maquina = %s" #comando sql delete
+    conn = conectar() #abre a conexão
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, (id_maquina,)) #executa o comando
+        conn.commit()
+        affected = cur.rowcount
+        cur.close()
+        return affected > 0  #retorna true se a maquina foi deletada
+    finally:
+        conn.close() #fecha a conexão
+
+
+# -- Listar Máquinas por lavanderia
+def listar_maquinas_por_lavanderia(id_lavanderia: int) -> List[Maquina]:
+   
+    sql = "SELECT id_maquina, id_lavanderia, codigo_maquina, tipo_maquina, status_maquina, capacidade FROM maquina WHERE id_lavanderia = %s" #comando sql select
+    conn = conectar() #abre a conexão
+    maquinas = []
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, (id_lavanderia,)) #executa o comando
+        for row in cur.fetchall():    #para cada linha cria um objeto maquina e adiciona ele na lista
+            maquinas.append(Maquina(*row))  
+        cur.close()
+        return maquinas  #retorna a lista de maquinas
+    finally:
+        conn.close() #fecha a conexão
+
+
+# -- Acessar uma Máquina especifica pelo id dela
+def obter_maquina_por_id(id_maquina: int) -> Optional[Maquina]:
+    
+    sql = "SELECT id_maquina, id_lavanderia, codigo_maquina, tipo_maquina, status_maquina, capacidade FROM maquina WHERE id_maquina = %s" #comando sql select
+    conn = conectar()  #abre conexão
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, (id_maquina,)) #executa o comando
+        row = cur.fetchone()
+        cur.close()
+        if row:
+            return Maquina(*row)   #retorna o objeto maquina
+        return None
+    finally:
+        conn.close()  #fecha a conexão
+
